7ab1a2f7489b2f28b8e9e602f85a29865ebc9d21
byungmin12 <kbm940526@naver.com>
2022-08-07 14:43:36 +0900

    lazyload의 단점 클릭 한 순간 모달 파일을 가지고 오고 이를 그려서 화면에 보여줌 이게 최초페이지만 로딩이 빨라지고 모달 자체는 느려진거임 그래서 클릭이 되기 전에 모달 파일을 로드 시켜줘야함 방법 두가지 !
    Line: 16
    마우스가 버튼위에 올려졌을때 모달 파일을 가지고 오는 방법
    
    Line: 13
    중요한 모든 파일을 가지고 온 뒤에 모달 파일을 가지고 오는 방법
    
    Line: 9
    보일러 템플릿을 없애기 위한 방법

--- performanceCh_2/src/App.js
+++ performanceCh_2/src/App.js
@@ -6,16 +6,28 @@ import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
 // import ImageModal from './components/ImageModal'
 
-const LazyImageModel = React.lazy(()=>import('./components/ImageModal'))
+function lazyWithPreload (importFunction){
+    const Component = React.lazy(importFunction)
+    Component.preload = importFunction
+    return Component
+}
+
+const LazyImageModel = lazyWithPreload(()=>import('./components/ImageModal'))
 
 function App() {
     const [showModal, setShowModal] = useState(false)
+    React.useEffect(()=>{
+        const _component = import('./components/ImageModal')
+    },[])
+    const handlerOnMouseLoad = () =>{
+        // const _component = import('./components/ImageModal')
+    }
 
     return (
         <div className="App">
             <Header />
             <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
+            <ButtonModal onClick={() => { setShowModal(true) }} onMouseEnter={handlerOnMouseLoad} >올림픽 사진 보기</ButtonModal>
             <SurveyChart />
             <Footer />
             <React.Suspense fallback={null}>
