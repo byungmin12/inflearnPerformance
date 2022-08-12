a08191d2f1390f4f2351d14346e0f19d87d39eb3
byungmin12 <kbm940526@naver.com>
2022-08-13 08:49:57 +0900

    이미지 확장자, 사이즈 최적화
    1. 사이즈를 css에서 정해놓은 사이즈 *2를 줘서 이미지 크기를 변경
    ( 화면이 뭉게지지 않는 선에서 변경시켜줘야함 고정값이 아님 ! )
    
    2. picture태그를 사용하여 확장자에 따라 분기를 나눠줌
    type="image/webp"를 주어 webp를 사용할 수 있는 환경이면 webp를 출력 아니면 img태그를 출력
    
    ( * picture태그의 source태그는 src가 아닌 srcset에 경로를 넣어줘야함 )
    
    3. webp를 사용할 때도 lazy loading를 해줘야함 이 때 img태그에 useRef를 걸고 previousSibling을 통해 이전 형제 태그를 잡아주고 srcset를 먹임

--- performanceCh_3/src/components/Card.js
+++ performanceCh_3/src/components/Card.js
@@ -1,28 +1,35 @@
 import React from 'react'
 
 function Card(props) {
-	let imgRef = React.useRef(null)
-	React.useEffect(()=>{
-		const callback = (entries, observer) => {
-			entries.forEach(entry => {
-				if(entry.isIntersecting){
-					entry.target.src = props.image
-					observer.unobserve(entry.target)
-				}
-			});
-		};
-		const options = {}
-		const intersectionOv = new IntersectionObserver(callback, options)
-		intersectionOv.observe(imgRef.current)
-	},[])
-	return (
-		<div className="Card text-center">
-			<img ref={imgRef} alt={"apply lazy loading"}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+    let imgRef = React.useRef(null)
+    React.useEffect(() => {
+        const callback = (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const tag = entry.target
+                    const siblingTag = tag.previousSibling
+                    siblingTag.함 = props.webp
+                    tag.src = props.image
+
+                    observer.unobserve(tag)
+                }
+            });
+        };
+        const options = {}
+        const intersectionOv = new IntersectionObserver(callback, options)
+        intersectionOv.observe(imgRef.current)
+    }, [])
+    return (
+        <div className="Card text-center">
+            <picture>
+                <source type="image/webp"/>
+                <img ref={imgRef} alt={"apply lazy loading"}/>
+            </picture>
+            <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+                {props.children}
+            </div>
+        </div>
+    )
 }
 
 export default Card
