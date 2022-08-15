bb379a00b9633a897e453ca563d6b7a1a7979b7c
byungmin12 <kbm940526@naver.com>
2022-08-15 17:53:16 +0900

    함수 최적화를 통해 병목현상 줄이기
    이미지 크기가 크면 클 수록 계산하는데 시간이 오래 걸리기 때문에 이미지를 캔버스에 그린 후 캔버스 크기를 줄인다음 데이터를 얻는다 . 그후 로직 처리

--- performanceCh_4/src/utils/getAverageColorOfImage.js
+++ performanceCh_4/src/utils/getAverageColorOfImage.js
@@ -17,23 +17,26 @@ export function getAverageColorOfImage(imgElement) {
     return averageColor;
   }
 
-  const width = (canvas.width =
-    imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width);
-  const height = (canvas.height =
-    imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height);
+  const width =
+    imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width
+  const height =
+    imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height
 
-  context.drawImage(imgElement, 0, 0);
+  canvas.width =width /3
+  canvas.height = height/3
+  context.drawImage(imgElement, 0, 0, canvas.width,canvas.height);
 
-  const imageData = context.getImageData(0, 0, width, height).data;
+  const imageData = context.getImageData(0, 0, canvas.width, canvas.height).data;
   const length = imageData.length;
 
-  for (let i = 0; i < length; i += 4) {
+
+  for (let i = 0; i < length; i += 4 * 10) {
     averageColor.r += imageData[i];
     averageColor.g += imageData[i + 1];
     averageColor.b += imageData[i + 2];
   }
 
-  const count = length / 4;
+  const count = length / (4 * 10)
   averageColor.r = ~~(averageColor.r / count); // ~~ => convert to int
   averageColor.g = ~~(averageColor.g / count);
   averageColor.b = ~~(averageColor.b / count);
