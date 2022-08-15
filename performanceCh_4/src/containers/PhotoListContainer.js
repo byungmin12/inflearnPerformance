7855beef45dc205bcbbe35aa2757dcd397fe0091
byungmin12 <kbm940526@naver.com>
2022-08-15 16:29:04 +0900

    상태값 변경 시 리렌더링 최소화

--- performanceCh_4/src/containers/PhotoListContainer.js
+++ performanceCh_4/src/containers/PhotoListContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import PhotoList from '../components/PhotoList';
 import { fetchPhotos } from '../redux/photos';
 
@@ -10,15 +10,15 @@ function PhotoListContainer() {
     dispatch(fetchPhotos());
   }, [dispatch]);
 
-  const { photos, loading } = useSelector(state => ({
-    photos:
-      state.category.category === 'all'
-        ? state.photos.data
-        : state.photos.data.filter(
-            photo => photo.category === state.category.category
-          ),
+  const { photos, loading, category } = useSelector(state => ({
+    photos: state.photos.data,
+    category : state.category.category,
     loading: state.photos.loading,
-  }));
+  }),shallowEqual);
+
+  const filteredPhotos = category==="all" ? photos : photos.filter(
+    photo => photo.category === category
+  )
 
   if (loading === 'error') {
     return <span>Error!</span>;
@@ -28,7 +28,7 @@ function PhotoListContainer() {
     return <span>loading...</span>;
   }
 
-  return <PhotoList photos={photos} />;
+  return <PhotoList photos={filteredPhotos} />;
 }
 
 export default PhotoListContainer;
